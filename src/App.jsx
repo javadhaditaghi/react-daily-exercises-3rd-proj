166b32dd2ea04c38eef91a5357eb263141f2e63c
javad <javadht1990a@gmail.com>
2024-02-12 23:21:45 +0330

    Create the project and make the header and index page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './assets/Header/Header'
+import Hero from './assets/Hero/Hero'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import "./Hero.css"
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+
+const Hero = () => {
+
+    return (
+        <section id="hero" className="hero">
+            <img src="../../../public/hero-bg.jpg" alt="" data-aos="fade-in" />
+            <Container>
+                <Row>
+                    <Col lg={10}>
+                        <h2 data-aos="fade-up" data-aos-delay="100">Welcome to Our Website</h2>
+                        <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap
+                        </p>
+                    </Col>
+                    <Col lg={5}>
+                        <form action="#" className="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
+                            <input type="text" className="form-control" placeholder="Enter email address" />
+                            <input type="submit" className="btn btn-primary" value="Sign up" />
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+
+    )
+}
+
+export default Hero
