ce475b92bd6d858e4720aa8dd8296e1dd52a5a0c
javad <javadht1990a@gmail.com>
2024-02-13 01:56:13 +0330

    Add the AOS package for animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from './assets/About-us/About-us'
 import Header from './assets/Header/Header'
 import Hero from './assets/Hero/Hero'
+import Services from './assets/Services/Services';
 
 function App() {
 
@@ -14,6 +15,9 @@ function App() {
           <Route path='/' element={<Header />}>
             <Route index element={<Hero />} />
             <Route path='about' element={<AboutUs />} />
+            <Route path='services' element={<Services />} />
+
+
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/assets/Services/Services.jsx
@@ -0,0 +1,40 @@
+import { Container, Col, Row } from "react-bootstrap"
+import "./Services.css"
+import jsonData1 from "./Services.json"
+import Aos from "aos";
+import { useEffect } from 'react';
+import 'aos/dist/aos.css';
+
+const Services = () => {
+    useEffect(() => {
+        Aos.init();
+    }, []);
+    return (
+        <section id="services" className="services">
+            <Container className="section-title" data-aos="fade-up">
+                <h2>Services</h2>
+                <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+            </Container>
+            <Container>
+                <Row className="gy-4">
+                    {jsonData1.map(service => (
+                        <Col lg={6} data-aos="fade-up" data-aos-delay="100" key={service.id}>
+                            <div className="service-item d-flex">
+                                <div className="icon flex-shrink-0"><i className={service.icon}></i></div>
+                                <div>
+                                    <h4 className="title"><a href="services-details.html" className="stretched-link">{service.title}</a></h4>
+                                    <p className="description">{service.description}</p>
+                                </div>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+
+            </Container>
+
+        </section>
+    )
+}
+
+
+export default Services
