ab52b00950657dba8a2bc5536517d54e20700857
javad <javadht1990a@gmail.com>
2024-02-13 21:55:47 +0330

    Add the Team section json file

--- src/assets/Team/Team.jsx
+++ src/assets/Team/Team.jsx
@@ -1,4 +1,5 @@
 import "./Team.css";
+import jsonData from "./Team.json"
 
 const Team = () => {
     return (
@@ -14,113 +15,33 @@ const Team = () => {
 
                 <div className="row gy-5">
 
-                    <div className="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="100">
-                        <div className="member-img">
-                            <img src="../../../public/team/team-1.jpg" className="img-fluid" alt="" />
-                            <div className="social">
-                                <a href="#"><i className="bi bi-twitter"></i></a>
-                                <a href="#"><i className="bi bi-facebook"></i></a>
-                                <a href="#"><i className="bi bi-instagram"></i></a>
-                                <a href="#"><i className="bi bi-linkedin"></i></a>
+                    {jsonData.map(member => (
+                        <div className="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="100">
+                            <div className="member-img">
+                                <img src={member.imageSrc} className="img-fluid" alt="" />
+                                <div className="social">
+                                    <a href="#"><i className="bi bi-twitter"></i></a>
+                                    <a href="#"><i className="bi bi-facebook"></i></a>
+                                    <a href="#"><i className="bi bi-instagram"></i></a>
+                                    <a href="#"><i className="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div className="member-info text-center">
+                                <h4>{member.name}</h4>
+                                <span>{member.position}</span>
+                                <p>{member.description}</p>
                             </div>
                         </div>
-                        <div className="member-info text-center">
-                            <h4>Walter White</h4>
-                            <span>Chief Executive Officer</span>
-                            <p>Aliquam iure quaerat voluptatem praesentium possimus unde laudantium vel dolorum distinctio dire flow
-                            </p>
-                        </div>
-                    </div>
 
-                    <div className="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="200">
-                        <div className="member-img">
-                            <img src="../../../public/team/team-2.jpg" className="img-fluid" alt="" />
-                            <div className="social">
-                                <a href="#"><i className="bi bi-twitter"></i></a>
-                                <a href="#"><i className="bi bi-facebook"></i></a>
-                                <a href="#"><i className="bi bi-instagram"></i></a>
-                                <a href="#"><i className="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div className="member-info text-center">
-                            <h4>Sarah Jhonson</h4>
-                            <span>Product Manager</span>
-                            <p>Labore ipsam sit consequatur exercitationem rerum laboriosam laudantium aut quod dolores exercitationem
-                                ut</p>
-                        </div>
-                    </div>
+                    ))}
 
-                    <div className="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="300">
-                        <div className="member-img">
-                            <img src="../../../public/team/team-3.jpg" className="img-fluid" alt="" />
-                            <div className="social">
-                                <a href="#"><i className="bi bi-twitter"></i></a>
-                                <a href="#"><i className="bi bi-facebook"></i></a>
-                                <a href="#"><i className="bi bi-instagram"></i></a>
-                                <a href="#"><i className="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div className="member-info text-center">
-                            <h4>William Anderson</h4>
-                            <span>CTO</span>
-                            <p>Illum minima ea autem doloremque ipsum quidem quas aspernatur modi ut praesentium vel tque sed facilis
-                                at qui</p>
-                        </div>
-                    </div>
 
-                    <div className="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="400">
-                        <div className="member-img">
-                            <img src="../../../public/team/team-4.jpg" className="img-fluid" alt="" />
-                            <div className="social">
-                                <a href="#"><i className="bi bi-twitter"></i></a>
-                                <a href="#"><i className="bi bi-facebook"></i></a>
-                                <a href="#"><i className="bi bi-instagram"></i></a>
-                                <a href="#"><i className="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div className="member-info text-center">
-                            <h4>Amanda Jepson</h4>
-                            <span>Accountant</span>
-                            <p>Magni voluptatem accusamus assumenda cum nisi aut qui dolorem voluptate sed et veniam quasi quam
-                                consectetur</p>
-                        </div>
-                    </div>
 
-                    <div className="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="500">
-                        <div className="member-img">
-                            <img src="../../../public/team/team-5.jpg" className="img-fluid" alt="" />
-                            <div className="social">
-                                <a href="#"><i className="bi bi-twitter"></i></a>
-                                <a href="#"><i className="bi bi-facebook"></i></a>
-                                <a href="#"><i className="bi bi-instagram"></i></a>
-                                <a href="#"><i className="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div className="member-info text-center">
-                            <h4>Brian Doe</h4>
-                            <span>Marketing</span>
-                            <p>Qui consequuntur quos accusamus magnam quo est molestiae eius laboriosam sunt doloribus quia impedit
-                                laborum velit</p>
-                        </div>
-                    </div>
 
-                    <div className="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="600">
-                        <div className="member-img">
-                            <img src="../../../public/team/team-6.jpg" className="img-fluid" alt="" />
-                            <div className="social">
-                                <a href="#"><i className="bi bi-twitter"></i></a>
-                                <a href="#"><i className="bi bi-facebook"></i></a>
-                                <a href="#"><i className="bi bi-instagram"></i></a>
-                                <a href="#"><i className="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div className="member-info text-center">
-                            <h4>Josepha Palas</h4>
-                            <span>Operation</span>
-                            <p>Sint sint eveniet explicabo amet consequatur nesciunt error enim rerum earum et omnis fugit eligendi
-                                cupiditate vel</p>
-                        </div>
-                    </div>
+
+
+
+
 
 
                 </div>
