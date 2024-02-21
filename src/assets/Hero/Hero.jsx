030f06aa68580715d9e5ab22880bad9ca358759c
javad <javadht1990a@gmail.com>
2024-02-21 18:56:20 +0330

    Add the input change handler

--- src/assets/Hero/Hero.jsx
+++ src/assets/Hero/Hero.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Aos from "aos";
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
+import jsonDataHero from "./../Blog/Blog.json"
 
 
 
@@ -12,20 +13,26 @@ const Hero = () => {
         Aos.init();
     }, []);
 
+
+
+    const handleInputChange = (event) => {
+        console.log(event.target.value);
+    }
+
     return (
         <section id="hero" className="hero">
             <img src="../../../public/hero-bg.jpg" alt="" data-aos="fade-in" />
             <Container>
                 <Row>
                     <Col lg={10}>
-                        <h2 data-aos="fade-up" data-aos-delay="100">Welcome to Our Website</h2>
-                        <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap
+                        <h2 data-aos="fade-up" data-aos-delay="100">We are blogger.com</h2>
+                        <p data-aos="fade-up" data-aos-delay="200">Search the blogs and find the one you need
                         </p>
                     </Col>
                     <Col lg={5}>
                         <form action="#" className="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
-                            <input type="text" className="form-control" placeholder="Enter email address" />
-                            <input type="submit" className="btn btn-primary" value="Sign up" />
+                            <input type="text" className="form-control" placeholder="How can we help you?" onChange={handleInputChange} />
+                            <input type="submit" className="btn btn-primary" value="Search" />
                         </form>
                     </Col>
                 </Row>
