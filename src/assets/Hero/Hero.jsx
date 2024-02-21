c0c0a502e4d95b9d31f7a3a5c1cc09da3a144196
javad <javadht1990a@gmail.com>
2024-02-21 19:46:58 +0330

    Add the search functionality to the hero

--- src/assets/Hero/Hero.jsx
+++ src/assets/Hero/Hero.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Aos from "aos";
 import 'aos/dist/aos.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import jsonDataHero from "./../Blog/Blog.json"
+import { Link } from "react-router-dom";
+import "../Team/Team.css";
 
 
 
@@ -13,11 +15,21 @@ const Hero = () => {
         Aos.init();
     }, []);
 
+    const [blogs, setBlogs] = useState()
+
 
 
     const handleInputChange = (event) => {
-        console.log(event.target.value);
+        setBlogs(event.target.value);
     }
+
+    const filteredBlogs = () => {
+        return jsonDataHero.filter(blog => blog.title.includes(blogs))
+    }
+
+    console.log(filteredBlogs())
+
+
 
     return (
         <section id="hero" className="hero">
@@ -34,9 +46,53 @@ const Hero = () => {
                             <input type="text" className="form-control" placeholder="How can we help you?" onChange={handleInputChange} />
                             <input type="submit" className="btn btn-primary" value="Search" />
                         </form>
+
                     </Col>
                 </Row>
+
             </Container>
+            <section id="blog" className="blog">
+
+                <Container data-aos="fade-up" data-aos-delay="100">
+
+                    <div className="posts-list">
+                        <Row className="gy-4">
+
+                            {filteredBlogs().map(blog => (
+                                <Col xl={4} lg={6} key={blog.id} className="mb-4">
+                                    <article>
+
+                                        <div className="post-img">
+                                            <img src={blog.imageSrc} alt="" className="img-fluid" />
+                                        </div>
+
+                                        <p className="post-category">{blog.category}</p>
+
+                                        <h2 className="title">
+                                            <Link to={`/Blog/${blog.id}`}>{blog.title}</Link>
+                                        </h2>
+
+                                        <div className="d-flex align-items-center">
+                                            <img src={blog.authorImg} alt="" className="img-fluid post-author-img flex-shrink-0" />
+                                            <div className="post-meta">
+                                                <p className="post-author">{blog.author}</p>
+                                                <p className="post-date">
+                                                    <time datetime="2022-01-01">{blog.date}</time>
+                                                </p>
+                                            </div>
+                                        </div>
+
+                                    </article>
+                                </Col>
+                            ))}
+                        </Row>
+                    </div>
+
+
+                </Container>
+
+            </section>
+
 
         </section>
 
