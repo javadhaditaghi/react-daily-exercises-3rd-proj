4b8cde96ad7ee8c40214beb53c0436f22a37512c
javad <javadht1990a@gmail.com>
2024-02-21 20:10:15 +0330

    Add the proper styling to the seatrched blogs

--- src/assets/Hero/Hero.jsx
+++ src/assets/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import "./Hero.css"
+import "../../../src/assets/Team/Team.css";
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Aos from "aos";
@@ -6,7 +7,6 @@ import 'aos/dist/aos.css';
 import { useEffect, useState } from "react";
 import jsonDataHero from "./../Blog/Blog.json"
 import { Link } from "react-router-dom";
-import "../Team/Team.css";
 
 
 
@@ -24,7 +24,7 @@ const Hero = () => {
     }
 
     const filteredBlogs = () => {
-        return jsonDataHero.filter(blog => blog.title.includes(blogs))
+        return jsonDataHero.filter(blogi => blogi.title.includes(blogs))
     }
 
     console.log(filteredBlogs())
@@ -32,25 +32,28 @@ const Hero = () => {
 
 
     return (
-        <section id="hero" className="hero">
-            <img src="../../../public/hero-bg.jpg" alt="" data-aos="fade-in" />
-            <Container>
-                <Row>
-                    <Col lg={10}>
-                        <h2 data-aos="fade-up" data-aos-delay="100">We are blogger.com</h2>
-                        <p data-aos="fade-up" data-aos-delay="200">Search the blogs and find the one you need
-                        </p>
-                    </Col>
-                    <Col lg={5}>
-                        <form action="#" className="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
-                            <input type="text" className="form-control" placeholder="How can we help you?" onChange={handleInputChange} />
-                            <input type="submit" className="btn btn-primary" value="Search" />
-                        </form>
+        <>
+            <section id="hero" className="hero">
+                <img src="../../../public/hero-bg.jpg" alt="" data-aos="fade-in" />
+                <Container>
+                    <Row>
+                        <Col lg={10}>
+                            <h2 data-aos="fade-up" data-aos-delay="100">We are blogger.com</h2>
+                            <p data-aos="fade-up" data-aos-delay="200">Search the blogs and find the one you need
+                            </p>
+                        </Col>
+                        <Col lg={5}>
+                            <form action="#" className="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
+                                <input type="text" className="form-control" placeholder="How can we help you?" onChange={handleInputChange} />
+                                <input type="submit" className="btn btn-primary" value="Search" />
+                            </form>
 
-                    </Col>
-                </Row>
+                        </Col>
+                    </Row>
 
-            </Container>
+                </Container>
+            </section>
+
             <section id="blog" className="blog">
 
                 <Container data-aos="fade-up" data-aos-delay="100">
@@ -93,8 +96,8 @@ const Hero = () => {
 
             </section>
 
+        </>
 
-        </section>
 
     )
 }
